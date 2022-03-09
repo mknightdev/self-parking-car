1d0d6b3ac59ee3bc2237e3f4f5450967fdb9118f
mknightdev <mknightdev@gmail.com>
2022-03-09 18:18:51 +0000

    agent car

--- Assets/Scripts/CarLocomotion.cs
+++ Assets/Scripts/CarLocomotion.cs
@@ -5,119 +5,73 @@ using UnityEngine;
 
 public class CarLocomotion : MonoBehaviour
 {
-    [Header("Car Park")]
-    public Transform parkingSlots;
-    public Transform goalParkingSlot;
+    // Wheels
+    [SerializeField] private WheelCollider frontRight;
+    [SerializeField] private WheelCollider frontLeft;
+    [SerializeField] private WheelCollider backRight;
+    [SerializeField] private WheelCollider backLeft;
 
-    private float horizontalInput;
-    private float verticalInput;
-    private float steerAngle;
-    private float currentBreakForce;
-    private bool isBreaking;
+    // Reference to wheel mesh
+    [SerializeField] private Transform frontRightTransform;
+    [SerializeField] private Transform frontLeftTransform;
+    [SerializeField] private Transform backRightTransform;
+    [SerializeField] private Transform backLeftTransform;
 
-    [Header("Car Settings")]
-    [SerializeField] private float motorForce;
-    [SerializeField] private float breakForce;
-    [SerializeField] private float maxAngle;
+    public float acceleration = 500f;
+    public float brakeForce = 300f; // How quickly the car comes to a stop
+    public float maxTurnAngle = 30f;
 
-    [SerializeField] private WheelCollider frontLCollider;
-    [SerializeField] private WheelCollider frontRCollider;
-    [SerializeField] private WheelCollider backLCollider;
-    [SerializeField] private WheelCollider backRCollider;
-
-    [SerializeField] private Transform frontLTransform;
-    [SerializeField] private Transform frontRTransform;
-    [SerializeField] private Transform backLTransform;
-    [SerializeField] private Transform backRTransform;
-
-
-    private void Start()
-    {
-        for (int i = 0; i < parkingSlots.childCount; i++)
-        {
-            // Check if gameobject is active
-            if (parkingSlots.GetChild(i).GetChild(0).gameObject.activeSelf)
-            {
-                // Calculate the distance for each parking slot
-                var newDistance = Vector3.Distance(this.transform.position, parkingSlots.GetChild(i).GetChild(0).position);
-                var distance = 0.0f;
-                if (distance == 0.0f || newDistance < distance)
-                {
-                    // Set the distance to the closest distance
-                    distance = newDistance;
-                    goalParkingSlot = parkingSlots.GetChild(i).GetChild(0);
-                }
-            }
-        }
-    }
+    private float currentAcceleration = 0.0f;
+    private float currentBrakeForce = 0.0f;
+    private float currentTurnAngle = 0.0f;
 
     private void FixedUpdate()
     {
-        GetInput();
-        HandleMotor();
-        HandleSteering();
-        UpdateWheels();
-    }
+        // W and S for forward and reverse
+        currentAcceleration = acceleration * Input.GetAxis("Vertical");
 
-    private void GetInput()
-    {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
-        isBreaking = Input.GetKey(KeyCode.Space);
-    }
-
-    private void HandleMotor()
-    {
-        frontLCollider.motorTorque = verticalInput * motorForce;
-        frontRCollider.motorTorque = verticalInput * motorForce;
-        breakForce = isBreaking ? breakForce : 0f;
-        if (isBreaking)
+        // Apply brake on space
+        if (Input.GetKey(KeyCode.Space))
         {
-            ApplyBreaking();
+            currentBrakeForce = brakeForce;
         }
-    }
-
-    private void ApplyBreaking()
-    {
-        Debug.Log("Braking!");
-        frontLCollider.brakeTorque = currentBreakForce;
-        frontRCollider.brakeTorque = currentBreakForce;
-        backLCollider.brakeTorque = currentBreakForce;
-        backRCollider.brakeTorque = currentBreakForce;
-    }
-
-    private void HandleSteering()
-    {
-        steerAngle = maxAngle * horizontalInput;
-        frontLCollider.steerAngle = steerAngle;
-        frontRCollider.steerAngle = steerAngle;
-
-    }
-
-    private void UpdateWheels()
-    {
-        UpdateSingleWheel(frontLCollider, frontLTransform);
-        UpdateSingleWheel(frontRCollider, frontRTransform);
-        UpdateSingleWheel(backLCollider, backLTransform);
-        UpdateSingleWheel(backRCollider, backRTransform);
-    }
-
-    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
-    {
-        Vector3 pos;
-        Quaternion rot;
-
-        wheelCollider.GetWorldPose(out pos, out rot);
-        wheelTransform.rotation = rot;
-        wheelTransform.position = pos;
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.CompareTag("goal"))
+        else
         {
-            // Give reward
-            Debug.Log("goal reached");
+            currentBrakeForce = 0.0f;
         }
+
+        // Apply acceleration to the front wheels
+        frontRight.motorTorque = currentAcceleration;
+        frontLeft.motorTorque = currentAcceleration;
+
+        frontRight.brakeTorque = currentBrakeForce;
+        frontLeft.brakeTorque = currentBrakeForce;
+        backRight.brakeTorque = currentBrakeForce;
+        backLeft.brakeTorque = currentBrakeForce;
+
+        // Steering
+        currentTurnAngle = maxTurnAngle * Input.GetAxis("Horizontal");
+        frontLeft.steerAngle = currentTurnAngle;
+        frontRight.steerAngle = currentTurnAngle;
+
+        UpdateWheel(frontRight, frontRightTransform);
+        UpdateWheel(frontLeft, frontLeftTransform);
+        UpdateWheel(backRight, backRightTransform);
+        UpdateWheel(backLeft, backLeftTransform);
+    }
+
+    void UpdateWheel(WheelCollider col, Transform trans)
+    {
+        // Get wheel collider state
+        Vector3 position;
+        Quaternion rotation;
+
+        // Gets the position and rotation of wheel collider
+        col.GetWorldPose(out position, out rotation);
+
+        // Set the wheel position and rotation based on collider
+        trans.position = position;
+        trans.rotation = rotation;
+
     }
 }
