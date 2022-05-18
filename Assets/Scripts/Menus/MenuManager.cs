cb466add8fd8f76cc02008e12f60a48191d0deb3
mknightdev <mknightdev@gmail.com>
2022-05-18 15:17:19 +0100

    quit button prefab; removed scripts that were no longer needed; first build

--- Assets/Scripts/Menus/MenuManager.cs
+++ Assets/Scripts/Menus/MenuManager.cs
@@ -9,4 +9,18 @@ public class MenuManager : MonoBehaviour
     {
         SceneManager.LoadScene(0);
     }
+
+    private void Update()
+    {
+        PlayerInput();
+    }
+
+    private void PlayerInput()
+    {
+        if (SceneManager.GetActiveScene().buildIndex != 0 && Input.GetKeyDown(KeyCode.Escape))
+        {
+            // Show/Hide escape menu
+            transform.GetChild(0).gameObject.SetActive(!transform.GetChild(0).gameObject.activeInHierarchy);
+        }
+    }
 }
--- Assets/Scripts/Menus/SceneLoader.cs
+++ Assets/Scripts/Menus/SceneLoader.cs
@@ -6,15 +6,10 @@ using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
     public static int carsToSpawn;
-    public GameObject escapeMenu;
 
     private void Awake()
     {
         DontDestroyOnLoad(this);
-    }
-    void Update()
-    {
-        PlayerInput();
     }
 
     public void SetCarsValue(int numOfCars)
@@ -30,28 +25,5 @@ public class SceneLoader : MonoBehaviour
     public void ReturnToMenu()
     {   
         SceneManager.LoadScene(0);
-    }
-
-    private void OnEnable()
-    {
-        Debug.Log("OnEnable called");
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-
-    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        Debug.Log("OnSceneLoaded: " + scene.name);
-        Debug.Log(mode);
-
-        escapeMenu = GameObject.Find("EscapeMenu");
-    }
-
-    private void PlayerInput()
-    {
-        if (SceneManager.GetActiveScene().buildIndex != 0 && Input.GetKeyDown(KeyCode.Escape))
-        {
-            // Show/Hide escape menu
-            escapeMenu.transform.GetChild(0).gameObject.SetActive(!escapeMenu.transform.GetChild(0).gameObject.activeInHierarchy);
-        }
     }
 }
